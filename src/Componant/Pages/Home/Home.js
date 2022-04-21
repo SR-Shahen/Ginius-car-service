a979473d9be7b910ca6cd32eea76164628a59bfc
SR-Shahen <s.r.shahenda@gmail.com>
2022-04-21 06:18:59 +0600

    helmate added

--- src/Componant/Pages/Home/Home.js
+++ src/Componant/Pages/Home/Home.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Home-Genius Car Service</title>
+            </Helmet>
             <h4>This is home welcome to home</h4>
         </div>
     );
--- src/Componant/Pages/Product/Product.js
+++ src/Componant/Pages/Product/Product.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
 const Product = () => {
     return (
         <div>
+            <Helmet>
+                <title>Product- Genius Car Service</title>
+            </Helmet>
             <h1>Ekhane sulov mulle sokol pokar khuchra o paikari ponno pawa jai</h1>
         </div>
     );
